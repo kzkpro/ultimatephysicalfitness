4a311f032a0cea706e67bab12b672a958a1a8c34
kzkpro <kishor.khambu@gmail.com>
2022-03-10 23:01:51 +0545

    first commit

---
+++ quotes.js
@@ -0,0 +1,528 @@
+var quotes = [
+	{
+		quote: "Wanting something is not enough. You must hunger for it. ",
+		author: "Les Brown"
+	},
+	{
+		quote: "The road to nowhere is paved with excuses.",
+		author: "Mark Bell"
+	},
+	{
+
+		quote: "The application of consistent, logical effort, over a prolonged period is the key to reaching your physical muscular potential.",
+		author: "Craig Cecil" 
+	},
+	{
+
+		quote: "It’s not about having time. It’s about making time.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "I hated every minute of training, but I said, don’t quit. Suffer now and live the rest of your life as a champion.",
+		author: "Muhammad Ali"
+	},
+	{
+
+		quote: "The wolf on the hill is not as hungry as the wolf climbing the hill.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "You can have results or excuses. Not both.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "The worst thing I can be is the same as everybody else.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "The question isn’t who is going to let me; it’s who is going to stop me.",
+		author:"Ayn Rand"
+	},
+	{
+
+		quote: "Blood, sweat, and respect. The first two you give and the last one you earn.",
+		author: "Dwayne Johnson"
+	},
+	{
+
+		quote: "Positive thinking can be contagious. Being surrounded by winners helps you develop into a winner.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "The muscle and the mind must become one. One without the other is zero.",
+		author: "Lee Haney"
+	},
+	{
+
+		quote: "Everybody wants to be a bodybuilder but don’t nobody want to lift heavy weights!",
+		author: "Ronnie Coleman"
+	},
+	{
+
+		quote: "There’s no secret formula. I lift heavy, work hard, and aim to be the best.",
+		author: "Ronnie Coleman"
+	},
+	{
+
+		quote: "Find YOUR Balance.",
+
+		author:"Kayla Rose Kotecki"
+	},
+	{
+
+		quote: "A bodybuilder is a sculptor who carries with him his own material.",
+		author: "Serge Nubret"
+	},
+	{
+
+		quote: "Pain is temporary, pride is forever.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "The last three or four reps is what makes the muscle grow. This area of pain divides the champion from someone who is not a champion.",
+
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "Bodybuilding isn’t 90 minutes in the gym. It’s a lifestyle.",
+		author: "Lee Priest"
+	},
+	{
+
+		quote: "Suffer the pain of self-discipline. Or suffer the pain of regret.",
+		author: "Jim Rohn"
+	},
+	{
+
+		quote: "To be a champion, you must act like a champion.",
+
+		author: "Lou Ferrigno"
+	},
+	{
+
+		quote: "A champion is someone who gets up when they can’t.",
+		author: "Jack Dempsey"
+	},
+	{
+
+		quote: "Half-reps may build your ego but they also build half a physique.",
+		author:"Craig Cecil"
+	},
+	{
+
+		quote: "Skill is only developed by hours and hours of work.",
+		author: "Usain Bolt"
+	},
+	{
+
+		quote: "Champions aren’t made in the gyms. Champions are made from something they have deep inside them-a desire, a dream, a vision.",
+		author: "Muhammad Ali"
+	},
+	{
+
+		quote: "Once you have commitment, you need the discipline and hard work to get you there.",
+		author: "Haile Gebrselassie"
+	},
+	{
+
+		quote: "Strong people are harder to kill than weak people and more useful in general.",
+		author: "Mark Rippetoe"
+	},
+	{
+
+		quote: "The first and greatest victory is to conquer self.",
+		author: "Plato"
+	},
+	{
+
+		quote: "You’ve got to get up every morning with determination if you’re going to go to bed with satisfaction.",
+		author: "George Lorimer"
+	},
+	{
+
+		quote: "Discipline is built by consistently performing small acts of courage.",
+		author: "Robin Sharma"
+	},
+	{
+
+		quote: "When you have a well-developed body and you're confident, you see people bending your way, wanting to be on your side, wanting to do things for you.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "If I want to be great I have to win the victory over myself…self-discipline.",
+		author: "Harry S. Truman"
+	},
+	{
+
+		quote: "Strength is the product of struggle. You must do what others don’t to achieve what others won't.",
+		author: "Henry Rollins"
+	},
+	{
+
+		quote: "Discipline is the bridge between your bodybuilding goals and bodybuilding success.",
+		author: "Felicity Luckey"
+	},
+	{
+
+		quote: "Biceps are like ornaments on a Christmas tree.",
+		author: "Ed Coan"
+	},
+	{
+
+		quote: "Self-discipline is about controlling your desires and impulses while staying focused on what needs to get done to achieve your goal.",
+		author: "Adam Sicinski"
+	},
+	{
+
+		quote: "It took me 20 years of hard training to get the physique I have today. What you need is what I had – belief in yourself!",
+		author: "Branch Warre"
+	},
+	{
+
+		quote: "Self-belief and hard work will always earn you success.",
+		author: "Virat Kohli"
+	},
+	{
+
+		quote: "Sell yourself short on nutrition and you’re selling yourself short on maximizing your physique development.",
+		author: "Ernie Taylor"
+	},
+	{
+
+		quote: "Success is the sum of small efforts repeated day-in and day-out.",
+		author: "Robert Collier"
+	},
+	{
+
+		quote: "I don't diet and exercise. I eat and train.",
+		author:"J.R. Rim"
+	},
+	{
+
+		quote: "When it gets difficult is often right before you succeed.",
+		author: "Jeffrey Walker"
+	},
+	{
+
+		quote: "Fortunately, there is a solution, and it’s not performing multiple sets of whatever cable Kegel exercise is being pushed as 'The Answer.' Just a little hard, smart, basic work.",
+		author: "Jim Wendle"
+	},
+	{
+
+		quote: "You were born to win. But to be a winner, you must plan to win, prepare to win, and expect to win.",
+		author: "Zig Ziglar"
+	},
+	{
+
+		quote: "Vision creates faith and faith creates willpower. With faith, there is no anxiety and no doubt – just absolute confidence in yourself.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "Haters don’t hate you, they hate themselves. Because you’re a reflection of what they wish to be. ",
+		author: "Unknown"
+	},
+	{
+
+		quote: "Intensity builds immensity.",
+		author: "Kevin Levrone"
+	},
+	{
+
+		quote: "My old routine was McDonald's on the way to the gym, coffee during my workout, Burger King and Copenhagen post-workout.",
+		author: "Dave Tate"
+	},
+	{
+
+		quote: "Only those who dare to fail greatly can ever achieve greatly.",
+		author: "Robert F. Kennedy"
+	},
+	{
+
+		quote: "Your love for what you do and willingness to push yourself where others aren't prepared to go is what will make you great.",
+		author: "Laurence Shahlaei"
+	},
+	{
+
+		quote: "Losers quit when they fail. Winners fail until they succeed.",
+		author: "Robert T. Kiyosaki"
+	},
+	{
+
+		quote: "Let go of toxic control, in order to regain healthy control.",
+		author: "Kayla Rose Kotecki"
+	},
+	{
+
+		quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+		author: "Winston Churchill"
+	},
+	{
+
+		quote: "For me, life is continuously being hungry. The meaning of life is not simply to exist, to survive, but to move ahead, to go up, to achieve, to conquer.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "When you hit failure, your workout has just begun.",
+		author: "Ronnie Coleman"
+	},
+	{
+
+		quote: "Bodybuilding is much like any other sport. To be successful, you must dedicate yourself 100% to your training, diet and mental approach.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "Failure doesn’t kill you… it increases your desire to make something happen.",
+		author: "Kevin Costner"
+	},
+	{
+
+		quote: "Tough times don’t last. Tough people do.",
+		author: "Robert H. Schuller"
+	},
+	{
+
+		quote: "If you think lifting weights is dangerous, try being weak. Being weak is dangerous.",
+		author: "Bret Contreras"
+	},
+	{
+
+		quote: "What is the point of being alive if you don’t at least try to do something remarkable?",
+		author: "John Green"
+	},
+	{
+
+		quote: "Courage doesn’t always roar. Sometimes courage is the quiet voice at the end of the day saying, 'I will try again tomorrow.'",
+		author: "Mary Anne Radmacher"
+	},
+	{
+
+		quote: "The body always fails first, not the mind. The trick is to make the mind work for you, not against you.",
+		author: "Arnold Schwarzenegger"
+	},
+	{
+
+		quote: "If you aren’t begging for rest. Then you aren’t training your best.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "There is only one thing that makes a dream impossible to achieve: the fear of failure.",
+		author: "Paulo Coelho"
+	},
+	{
+
+		quote: "Some are born strong and others are made strong.",
+		author: "J.R. Rim"
+	},
+	{
+
+		quote: "People who avoid failure also avoid success.",
+		author: "Robert T. Kiyosaki"
+	},
+	{
+
+		quote: "Don’t measure yourself by what you have accomplished, but by what you should have accomplished with your ability.",
+		author: "John Wooden"
+	},
+	{
+
+		quote: "Don’t have $100.00 shoes and a 10 cent squat.",
+		author: "Louie Simmons"
+	},
+	{
+
+		quote: "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude.",
+		author: "Thomas Jefferson"
+	},
+	{
+
+		quote: "I'm the strongest bodybuilding who ever lived, I think.",
+		author: "Franco Columbu"
+	},
+	{
+
+		quote: "Remember - If you want to beat the man, you've gotta out-eat the man!",
+		author: "JM Blakely"
+	},
+	{
+
+		quote: "Discipline is the bridge between goals and accomplishments.",
+		author: "Jim Rohn"
+	},
+	{
+
+		quote: "I don’t do this to be healthy; I do this to get big muscles.",
+		author: "Marcus Ruhl"
+	},
+	{
+
+		quote: "The barbells and dumbbells you hold in your hands and the way you use them have stories to tell.",
+		author:"Craig Cecil"
+	},
+	{
+
+		quote: "There is no such thing as overtraining, just under nutrition and undersleeping.",
+		author: "The Barbarian brothers"
+	},
+	{
+
+		quote: "Don’t wish it were easier. Wish you were better. ",
+		author: "Jim Rohn"
+	},
+	{
+
+		quote: "I'm not sold on one diet philosophy. I'm sold on whatever will work for you.",
+		author: "Dave Tate"
+	},
+	{
+
+		quote: "Rule of thumb: Eat for what you’re going to be doing and not for what you have done. Don’t take in more than you’re willing to burn off.",
+		author:"Lee Haney"
+	},
+	{
+
+		quote: "I'm not the kind of guy who tries to run between the drops. Sometimes you gotta get a little wet to reach your destination",
+		author: "Erik Fankhouser"
+	},
+	{
+
+		quote: "You have to be at your strongest when you’re feeling at your weakest.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "Discipline is doing what you hate to do, but nonetheless doing it like you love it.",
+		author: "Mike Tyson"
+	},
+	{
+
+		quote: "Strength does not come from physical capacity. It comes from an indomitable will.",
+		author: "Mahatma Gandhi"
+	},
+	{
+
+		quote: "Learn it all, then forget it all.",
+		author: "Bruce Lee"
+	},
+	{
+
+		quote: "If you lack motivation, I can't help you with that. That's your problem to solve.",
+		author: "Stan Efferding"
+	},
+	{
+
+		quote: "Mediocre athletes that tried like hell to get good are the best coaches.",
+		author: "Mark Rippetoe"
+	},
+	{
+
+		quote: "Pain is temporary. Quitting lasts forever.",
+		author: "Lance Armstrong"
+	},
+	{
+
+		quote: "Most champions are built by punch the clock workouts rather than extraordinary efforts.",
+		author: "Dan John"
+	},
+	{
+
+		quote: "If I have to die tonight, if this weight is going to kill me tonight, so be it! I’m dying where I wanna be…",
+		author: "Kai Greene"
+	},
+	{
+
+		quote: "There's more to life than training, but training is what puts more in your life.",
+		author: "Brooks Kubik"
+	},
+	{
+
+		quote: "I would like to be the first man in the gym business to throw out my scale. If you don’t like what you see in the mirror, what difference does it make what the scale says?",
+		author: "Vince Gironda"
+	},
+	{
+
+		quote: "Come in the gym like you’re going to war.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "Don't ever believe you have nothing to lose. There's always something to be lost when you change something or try something new. Nothing is ever free. Not even experience.",
+		author: "Paul Carter"
+	},
+	{
+
+		quote: "There is no strength without struggle.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "Stimulate don’t annihilate.",
+		author: "Lee Haney"
+	},
+	{
+
+		quote: "Being negative and lazy is a disease that leads to pain, hardships, depression, poor health and failure. Be proactive  to achieve success!",
+		author:"Unknown"
+	},
+	{
+
+		quote: "Push yourself because no one else is going to do it.",
+		author: "Unknown"
+	},
+	{
+
+		quote: "There is no reason to be alive if you can't do the deadlift!",
+		author: "Jon Pall Sigmarsson"
+	},
+	{
+
+		quote: "I don’t eat for taste, I eat for function. ",
+		author: "Jay Cutler"
+	},
+	{
+
+		quote: "The Iron never lies to you. You can walk outside and listen to all kinds of talk, get told that you're a god. The Iron will always kick you the real deal. The Iron is the great reference point, the all-knowing perspective giver. The Iron is always there like a beacon in the pitch black. I have found the Iron to be my greatest friend. ",
+		author: "Henry Rollins"
+	},
+	{
+
+		quote: "Trust me, if you do an honest 20 rep program, at some point Jesus will talk to you. On the last day of the program, he asked if he could work in.",
+		author: "Mark Rippetoe"
+	},
+	{
+
+		quote: "At the end of the day it's not a weight contest, it's a visual contest. And it doesn't matter what you say about your weight, if you don't look that big then you don't look that big.",
+		author: "Dorian Yates"
+	},
+	{
+
+		quote: "I never think about losing - only the next weight I train.",
+		author: "Lou Ferrigno"
+	},
+	{
+
+		quote: "It's a rare individual who lets themselves be steered by what they feel is their own passion.",
+		author: "Dave Tate"
+	},
+	{
+
+		quote: "They can crack jokes. They can sit back and analyze and criticize and make all the fun they want. But I’m living my life, I’m doing it and pulling my weight. What are you doing?",
+		author: "Kai Greene"
+	},
+	{
+
+		quote: "On the Internet, everyone squats. In real life, the squat rack is always empty. You figure out what this means.",
+		author: "Steve Shaw"
+	}
+];
